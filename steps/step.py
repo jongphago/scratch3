316657e2516a66d210d65302f6b84f98c8f39733
jongphago <jonghyun.d.kim@gmail.com>
2021-07-13 05:50:13 +0000

    Step 06: 수동 역전파

--- steps/step.py
+++ steps/step.py
@@ -7,6 +7,8 @@ class Variable:
         if type(data) != np.ndarray:
             data = np.array(data)
         self.data = data
+        # 06
+        self.grad = None
 
 # 02
 class Function:
@@ -14,24 +16,41 @@ class Function:
         x = input.data
         y = self.forward(x) # 구체적인 계산은 forward 메서드에서 한다
         output = Variable(y)
+        # 06
+        self.input = input # 입력 변수를 기억(보관)한다.
         return output
-
     def forward(self, x):
+        raise NotImplementedError()
+    def backward(self, gy):
         raise NotImplementedError()
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = np.power(x, 2.)
+        return y
+    # 06
+    def backward(self, gy):
+        x = self.input.data
+        gx = (2. * x) * gy
+        return gx
 
 # 03
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
+    # 06
+    def backward(self, gy):
+        x = self.input.data
+        gx = (np.exp(x)) * gy
+        return gx
 
 # 04 
-def numerical_diff(f, x, eps=1e-4):
+def numerical_diff(f, x:Variable, eps=1e-4):
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
+
+# 06

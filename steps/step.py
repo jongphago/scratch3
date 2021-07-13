a0e4866c6e1761d584b9f9fb14d45ab2ac389410
jongphago <jonghyun.d.kim@gmail.com>
2021-07-13 10:08:40 +0000

    Step 09: 함수를 더 편리하게

--- steps/step.py
+++ steps/step.py
@@ -4,8 +4,10 @@ import numpy as np
 # 01
 class Variable:
     def __init__(self, data):
-        if type(data) != np.ndarray:
-            data = np.array(data)
+        # 09
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
         self.data = data
         # 06
         self.grad = None
@@ -23,6 +25,9 @@ class Variable:
             # x.backward() # 하나 앞 변수의 backward 메서드를 호출(재귀)
         # 08
         '반복문을 이용한 구현'
+        # 09
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
         func = [self.creator]
         while func:
             f = func.pop()  # 함수를 가져온다.
@@ -36,7 +41,8 @@ class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x) # 구체적인 계산은 forward 메서드에서 한다
-        output = Variable(y)
+        # 09
+        output = Variable(as_array(y))
         # 07
         output.set_creator(self) # 출력 변수에 창조자를 설정한다.
         # 06
@@ -78,4 +84,16 @@ def numerical_diff(f, x:Variable, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
-# 06
+# 09
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
